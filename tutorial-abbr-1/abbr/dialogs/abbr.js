b89ee2045cbc1f3f4d73a5ad2c43cd394ff5d542
AnnaTomanek <a.tomanek@cksource.com>
2014-09-10 15:28:45 +0200

    Abbr tutorial part 1 files added.

---
+++ tutorial-abbr-1/abbr/dialogs/abbr.js
@@ -0,0 +1,83 @@
+/**
+ * The abbr plugin dialog window definition.
+ *
+ * Created out of the CKEditor Plugin SDK:
+ * http://docs.ckeditor.com/#!/guide/plugin_sdk_sample_1
+ */
+
+// Our dialog definition.
+CKEDITOR.dialog.add( 'abbrDialog', function( editor ) {
+	return {
+
+		// Basic properties of the dialog window: title, minimum size.
+		title: 'Abbreviation Properties',
+		minWidth: 400,
+		minHeight: 200,
+
+		// Dialog window content definition.
+		contents: [
+			{
+				// Definition of the Basic Settings dialog tab (page).
+				id: 'tab-basic',
+				label: 'Basic Settings',
+
+				// The tab content.
+				elements: [
+					{
+						// Text input field for the abbreviation text.
+						type: 'text',
+						id: 'abbr',
+						label: 'Abbreviation',
+
+						// Validation checking whether the field is not empty.
+						validate: CKEDITOR.dialog.validate.notEmpty( "Abbreviation field cannot be empty." )
+					},
+					{
+						// Text input field for the abbreviation title (explanation).
+						type: 'text',
+						id: 'title',
+						label: 'Explanation',
+						validate: CKEDITOR.dialog.validate.notEmpty( "Explanation field cannot be empty." )
+					}
+				]
+			},
+
+			// Definition of the Advanced Settings dialog tab (page).
+			{
+				id: 'tab-adv',
+				label: 'Advanced Settings',
+				elements: [
+					{
+						// Another text field for the abbr element id.
+						type: 'text',
+						id: 'id',
+						label: 'Id'
+					}
+				]
+			}
+		],
+
+		// This method is invoked once a user clicks the OK button, confirming the dialog.
+		onOk: function() {
+
+			// The context of this function is the dialog object itself.
+			// http://docs.ckeditor.com/#!/api/CKEDITOR.dialog
+			var dialog = this;
+
+			// Create a new <abbr> element.
+			var abbr = editor.document.createElement( 'abbr' );
+
+			// Set element attribute and text by getting the defined field values.
+			abbr.setAttribute( 'title', dialog.getValueOf( 'tab-basic', 'title' ) );
+			abbr.setText( dialog.getValueOf( 'tab-basic', 'abbr' ) );
+
+			// Now get yet another field value from the Advanced Settings tab.
+			var id = dialog.getValueOf( 'tab-adv', 'id' );
+			if ( id )
+				abbr.setAttribute( 'id', id );
+
+			// Finally, insert the element at the editor caret position.
+			editor.insertElement( abbr );
+		}
+	};
+});
---
+++ tutorial-abbr-1/abbr/plugin.js
@@ -0,0 +1,36 @@
+/**
+ * Basic sample plugin inserting abbreviation elements into the CKEditor editing area.
+ *
+ * Created out of the CKEditor Plugin SDK:
+ * http://docs.ckeditor.com/#!/guide/plugin_sdk_sample_1
+ */
+
+// Register the plugin within the editor.
+CKEDITOR.plugins.add( 'abbr', {
+
+	// Register the icons.
+	icons: 'abbr',
+
+	// The plugin initialization logic goes inside this method.
+	init: function( editor ) {
+
+		// Define an editor command that opens our dialog window.
+		editor.addCommand( 'abbr', new CKEDITOR.dialogCommand( 'abbrDialog' ) );
+
+		// Create a toolbar button that executes the above command.
+		editor.ui.addButton( 'Abbr', {
+
+			// The text part of the button (if available) and the tooltip.
+			label: 'Insert Abbreviation',
+
+			// The command to execute on click.
+			command: 'abbr',
+
+			// The button placement in the toolbar (toolbar group name).
+			toolbar: 'insert'
+		});
+
+		// Register our dialog file -- this.path is the plugin folder path.
+		CKEDITOR.dialog.add( 'abbrDialog', this.path + 'dialogs/abbr.js' );
+	}
+});
