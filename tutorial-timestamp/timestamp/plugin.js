6edc819079b979dd5101a4fa7222bcdff8ffbc30
AnnaTomanek <a.tomanek@cksource.com>
2014-09-15 15:31:59 +0200

    Timestamp plugin tutorial files added.

---
+++ tutorial-timestamp/timestamp/plugin.js
@@ -0,0 +1,36 @@
+/**
+ * Basic sample plugin inserting current date and time into the CKEditor editing area.
+ *
+ * Created out of the CKEditor Plugin SDK:
+ * http://docs.ckeditor.com/#!/guide/plugin_sdk_intro
+ */
+
+// Register the plugin within the editor.
+CKEDITOR.plugins.add( 'timestamp', {
+
+	// Register the icons. They must match command names.
+	icons: 'timestamp',
+
+	// The plugin initialization logic goes inside this method.
+	init: function( editor ) {
+
+		// Define the editor command that inserts a timestamp.
+		editor.addCommand( 'insertTimestamp', {
+
+			// Define the function that will be fired when the command is executed.
+			exec: function( editor ) {
+				var now = new Date();
+
+				// Insert the timestamp into the document.
+				editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
+			}
+		});
+
+		// Create the toolbar button that executes the above command.
+		editor.ui.addButton( 'Timestamp', {
+			label: 'Insert Timestamp',
+			command: 'insertTimestamp',
+			toolbar: 'insert'
+		});
+	}
+});
